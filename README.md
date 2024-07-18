bbbb05929e95208f298d7ebebb7e94ec30e26676
Laura Castaño <lauracastadiaz@gmail.com>
2024-07-18 18:15:52 +0200

    subimos proyecto incompleto

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Magiclima 
-### Aplicación web del tiempo
+
+Magiclima es una aplicación web que proporciona información meteorológica en tiempo real para la ubicación actual del usuario y permite buscar el clima de otras ciudades. La aplicación utiliza la API de OpenWeatherMap para obtener los datos meteorológicos y los presenta de manera clara y visualmente atractiva.
+
+## Funcionalidades
+
+- **Detección de ubicación actual:** Al cargar la página, la aplicación obtiene automáticamente la ubicación actual del usuario y muestra la información meteorológica correspondiente.
+- **Búsqueda por ciudad:** Los usuarios pueden buscar el clima de cualquier ciudad ingresando su nombre en la barra de búsqueda.
+- **Visualización dinámica:** Muestra la temperatura, descripción del clima, velocidad del viento y un icono animado que representa las condiciones meteorológicas actuales.
+
+## Tecnologías utilizadas
+
+- **HTML5:** Estructura de la aplicación web.
+- **CSS3 y Bootstrap:** Diseño y estilos responsivos.
+- **JavaScript:** Lógica de la aplicación.
+- **API de OpenWeatherMap:** Obtención de datos meteorológicos.
+
+## Créditos
+- Desarrollador: Laura Castaño.
+
+- Íconos animados: Proporcionados por animated icons provider
---
+++ app.js
@@ -0,0 +1,108 @@
+window.addEventListener('load', ()=> {
+    let lon;
+    let lat;
+
+    let temperaturaValor = document.getElementById('temperatura-valor');
+    let temperaturaDescripcion= document.getElementById('temperatura-descripcion');
+
+    let ubicacion = document.getElementById('ubicacion');
+   // let icono= document.getElementById('icono');
+    let iconoAnimado= document.getElementById('icono-animado');
+
+    let vientoVelocidad = document.getElementById('viento-velocidad');
+    
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition( posicion => {
+            //console.log(posicion.coords.latitude);
+            lon= posicion.coords.longitude;
+            lat= posicion.coords.latitude;
+
+            //ubicacion actual
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=905603ece103542c1f38b332d691a7b0`
+        
+            //ubicacion por ciudad
+            //const url = `https://api.openweathermap.org/data/2.5/weather?q=Madrid&lang=es&units=metric&appid=905603ece103542c1f38b332d691a7b0`
+            //console.log(url);
+
+            fetch(url)
+            .then( response => {return response.json() })
+            .then( data => { 
+                console.log(data)
+                let temp = Math.round(data.main.temp)
+                temperaturaValor.textContent = `${temp}°C`;
+
+                let desc= data.weather[0].description;
+                temperaturaDescripcion.textContent= desc.toUpperCase();
+                
+                ubicacion.textContent = data.name;
+
+                vientoVelocidad.textContent = `${data.wind.speed} m/s`
+
+
+
+                
+
+                //para iconos estáticos
+                // let iconId= data.weather[0].icon;
+                // const urlIcon = `https://openweathermap.org/img/wn/${iconId}.png`
+                
+                // let img= document.createElement('img');
+                // img.src = urlIcon;
+                // icono.textContent = '';
+                // icono.appendChild(img);
+
+                //para iconos animados
+
+
+                switch(data.weather[0].main) {
+                    case 'Clear':
+                        iconoAnimado.src = 'animated/day.svg';
+                        console.log('LIMPIO');
+                        break;
+                    case 'Clouds':
+                        iconoAnimado.src = 'animated/cloudy-day-1.svg';
+                        console.log('CON NUBES');
+                        break;
+                    case 'Thunderstorm':
+                        iconoAnimado.src = 'animated/thunder.svg';
+                        console.log('TORMENTA');
+                        break;
+                    case 'Drizzle':
+                        iconoAnimado.src = 'animated/rainy-2.svg';
+                        console.log('LLOVIZNA');
+                        break;
+                    case 'Rain':
+                        iconoAnimado.src = 'animated/rainy-7.svg';
+                        console.log('LLUVIA');
+                        break;
+                    case 'Snow':
+                        iconoAnimado.src = 'animated/snowy-6.svg';
+                        console.log('NIEVE');
+                        break;
+                    case 'Atmosphere':
+                        iconoAnimado.src = 'animated/weather.svg';
+                        console.log('ATMOSFERA');
+                        break;
+                    default:
+                        iconoAnimado.src='animated/cloudy-day-1.svg'
+                        console.log('por defecto')
+                }
+
+            })
+            .catch( error => {
+                console.log(error)
+            })
+
+        
+        })
+
+
+
+    }
+
+    
+
+
+
+
+});
